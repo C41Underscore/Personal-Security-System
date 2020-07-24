16d9ce5c6e7c29f367e39133a88b8484a9e2dbf3
C41Underscore <callum.methold@sky.com>
2020-07-24 12:33:29 +0100

    Basic auth and upload working

--- google_drive_upload.py
+++ google_drive_upload.py
@@ -1,16 +1,8 @@
-from googleapiclient.discovery import build
-from httplib2 import Http
-from oauth2client import file, client, tools
+import google_drive_auth
 
-SCOPES = "https://www.googleapis.com/auth/drive"
+def upload():
+    drive = google_drive_auth.authenticate()
+    file1 = drive.CreateFile({"title":"test-image.jpg"})
+    file1.SetContentFile("test-image.jpg")
+    file1.Upload()
 
-CREDENTIALS = "credentials.json"
-
-store = file.Storage("storage.json")
-creds = store.get()
-if not creds or creds.invalid:
-    flow = client.flow_from_clientsecrets(CREDENTIALS, SCOPES)
-    creds = tools.run_flow(flow, store)
-
-SERVICE = build("drive", "v3", http=creds.authorize(Http()))
-print("Service end point created", SERVICE)
