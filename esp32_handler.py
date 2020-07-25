2388e322dbe008b28ac9ddc26dadf2bf998c17fb
C41Underscore <callum.methold@sky.com>
2020-07-25 16:02:52 +0100

    TODO list created and minor changes made

--- esp32_handler.py
+++ esp32_handler.py
@@ -2,7 +2,7 @@ from PIL import Image
 import urllib.request
 from string import Template
 
-class ESP32CAM_Interface:
+class esp32CamInterface:
 
     template_url = Template("http://${ip_address}/${image_type}")
 
@@ -17,12 +17,12 @@ class ESP32CAM_Interface:
         #     image_type=("cam-hi.jpg" if high_quality else "cam-lo.jpg")
         # )))
         if high_quality:
-            image = Image.open(urllib.request.urlopen(ESP32CAM_Interface.template_url.substitute(
+            image = Image.open(urllib.request.urlopen(esp32CamInterface.template_url.substitute(
                 ip_address=self.ip_address,
                 image_type="cam-hi.jpg"
             )))
         else:
-            image = Image.open(urllib.request.urlopen(ESP32CAM_Interface.template_url.substitute(
+            image = Image.open(urllib.request.urlopen(esp32CamInterface.template_url.substitute(
                 ip_address=self.ip_address,
                 image_type="cam-lo.jpg"
             )))
--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 import google_drive_auth
 import google_drive_upload
-from esp32_handler import ESP32CAM_Interface
+from esp32_handler import esp32CamInterface
+from time import sleep
+
+#TODO - Create the main framework of the program, going to have to wait for better internet to use the ESPs
+#TODO - Structure the code to make it efficient and readable
+#TODO - Create a test set of photos to test google drive uploads
+#TODO - Play around with pydrive and get photo uploads working
+#TODO - Play around with pydrive and get video upload working (use opencv and test with webcam)
+#TODO - Plan how the system will be managed.  What will trigger cameras, what extra physical components will be needed, and how will the camera be maintained
+#TODO - Plan how the file system will be organised
+#TODO - Get email notifications working for specific triggers (TBD)
+#TODO - Create a script to empty the google drive file regulary to prevent it from becoming full
 
 
 def main(testing):
     drive = google_drive_auth.authenticate()
-    esp32cam = ESP32CAM_Interface(1, "${IP_ADDRESS}", "Living Room")
+    esp32cam = esp32CamInterface(1, "${IP_ADDRESS}", "Living Room")
     if testing:
         for i in range(0, 10):
             esp32cam.takeImage(False)
+            sleep(5)
 
 
 if __name__ == "__main__":
