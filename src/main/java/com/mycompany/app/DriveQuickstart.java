b184bad38e66518a15a09fdad3c86e48fb4974a2
C41Underscore <callum.methold@sky.com>
2021-07-23 19:15:50 +0100

    google example code running, now need to examine and create google drive class for project

--- src/main/java/com/mycompany/app/DriveQuickstart.java
+++ src/main/java/com/mycompany/app/DriveQuickstart.java
@@ -8,6 +8,7 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.drive.Drive;
@@ -24,7 +25,7 @@ public class DriveQuickstart {
 
     private static final String APPLICATION_NAME = "Google Drive API Java Quickstart";
 
-    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
 
     // Directory to store user credentials for this application.
     private static final java.io.File CREDENTIALS_FOLDER //
